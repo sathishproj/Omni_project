4ac2131543a21ad45e13904bb8367f0f0aebf33a
nani <pallesathish4044@gmail.com>
2025-04-02 16:40:26 +0530

    our branches changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  locations_details = [
+    {
+      id: 1, location_name: 'Kothapet', img: 'omni_kothapet.png'
+    },
+    {
+      id: 2, location_name: 'Kukatpally', img: 'omni_kukatpally.png'
+    },
+    {
+      id: 3, location_name: 'UDAI OMNI - Nampally', img: 'udai_omni.png'
+    },
+    {
+      id: 4, location_name: 'Vizag', img: 'omni_vizag.png'
+    },
+    {
+      id: 5, location_name: 'Giggles Vizag', img: 'giggles_vizag_building.png'
+    },
+    {
+      id: 6, location_name: 'Kurnool', img: 'kurnool_location.png'
+    }
+  ]
 }

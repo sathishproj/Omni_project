59ac48a9dc279db4ac97024fd6dc018978f941a0
nani <pallesathish4044@gmail.com>
2025-04-18 15:30:33 +0530

    fixed surgeries changes

--- src/app/fixed-surgical-packages/fixed-surgical-packages.component.ts
+++ src/app/fixed-surgical-packages/fixed-surgical-packages.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,6 +9,20 @@ import { Component } from '@angular/core';
 export class FixedSurgicalPackagesComponent {
   locations: string[] = ['All Packages', 'Kothapet', 'Kukatpally', 'Nampally', 'Vizag', 'Kurnool'];
   selected: string = 'All Packages';
+  surgicalPackages: { [key: string]: any[] } = {};
+  packages: any[] = [];
+
+  fixed_surgical_paclages = [
+    {
+      id:1,
+      name: 'Knee Replacement Surgery',
+      price: '1,49,000/-',
+      img: 'assets/images/knee-thumb.jpg',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio...'
+    },
+
+  ]
+
   surgeryPackages = Array(9).fill({
     name: 'Knee Replacement Surgery',
     price: '1,49,000/-',
@@ -15,8 +30,29 @@ export class FixedSurgicalPackagesComponent {
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio...'
   });
 
+constructor(private http: HttpClient) {}
+ngOnInit(): void {
+  this.getFixedSurgicalJsonData();
+}
 
-
+getFixedSurgicalJsonData(){
+  this.http.get('assets/json_data_files/fixed_surgical_packages.json').subscribe((res: any) => {
+    console.log(res,'res...');
+    this.surgicalPackages = res;
+    this.updatePackages();
+  });
+}
+updatePackages(): void {
+  if (this.selected === 'All Packages') {
+    this.packages = Object.values(this.surgicalPackages).flat();
+    console.log(this.packages,'packages..');
+    
+  } else {
+    this.packages = this.surgicalPackages[this.selected] || [];
+  }
+}
   selectLocation(location: string) {
+    this.selected = location
+    this.updatePackages();    
   }
 }
